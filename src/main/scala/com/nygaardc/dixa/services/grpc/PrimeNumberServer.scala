8dca3c2d242c6b583086a913cb47648d18812470
Christian Nygaard <chr@nygaardc.com>
2020-03-01 16:50:36 +0100

    Remove unused code

--- src/main/scala/com/nygaardc/dixa/services/grpc/PrimeNumberServer.scala
+++ src/main/scala/com/nygaardc/dixa/services/grpc/PrimeNumberServer.scala
@@ -1,20 +1,14 @@
 package com.nygaardc.dixa.services.grpc
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.{Http, HttpConnectionContext}
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-import akka.stream.{ActorMaterializer, Materializer}
+import akka.http.scaladsl.{Http, HttpConnectionContext}
 import com.nygaardc.dixa.grpc.PrimeNumberServiceHandler
-import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object PrimeNumberServer {
   def main(args: Array[String]): Unit = {
-//    val conf = ConfigFactory
-//      .parseString("akka.http.server.preview.enable-http2 = on")
-//      .withFallback(ConfigFactory.defaultApplication())
-//    val system = ActorSystem("PrimeNumber", conf)
     val system = ActorSystem("PrimeNumber")
     new PrimeNumberServer(system).run()
   }
@@ -23,7 +17,6 @@ object PrimeNumberServer {
 class PrimeNumberServer(system: ActorSystem) {
   def run(): Future[Http.ServerBinding] = {
     implicit val sys: ActorSystem = system
-    implicit val mat: Materializer = ActorMaterializer()
     implicit val ec: ExecutionContext = sys.dispatcher
 
     val service: HttpRequest => Future[HttpResponse] = PrimeNumberServiceHandler(new PrimeNumberServiceImlp())
