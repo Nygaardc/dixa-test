be6705a6030db42844101479e88165e23c6134be
Christian Nygaard <chr@nygaardc.com>
2020-03-01 16:50:13 +0100

    Make prime number generation truly streaming

--- src/main/scala/com/nygaardc/dixa/services/grpc/PrimeNumberGenerator.scala
+++ src/main/scala/com/nygaardc/dixa/services/grpc/PrimeNumberGenerator.scala
@@ -1,24 +1,13 @@
 package com.nygaardc.dixa.services.grpc
 
-import scala.collection.mutable
-
 class PrimeNumberGenerator {
-  def getPrimes(n: Int): Stream[Int] = {
-//    if (n < 2) throw new BadInput("Input cannot be < 2")
-    val indexOfPrimes = mutable.ArrayBuffer.fill((n + 1) / 2)(1)
 
-    val end = Math.sqrt(n).toInt // optimization: early stopping
-    for (i <- 3 to n by 2 if i <= end) { // generate odd numbers
-      for (nonPrime <- i * i to n by 2 * i) { // identify multiples of i as non prime
-        indexOfPrimes.update(nonPrime / 2, 0)
-      }
-    }
+  private def sieve(s: Stream[Int]): Stream[Int] = {
+    s.head #:: sieve(s.tail filter (_ % s.head != 0))
+  }
 
-    val primes = for (
-      i <- indexOfPrimes.indices if indexOfPrimes(i) == 1)
-      yield 2 * i + 1
-
-    2 +: (primes tail).toStream
+  def getPrimes: Stream[Int] = {
+    sieve(Stream.from(2))
   }
 
 }
--- src/main/scala/com/nygaardc/dixa/services/grpc/PrimeNumberServiceImlp.scala
+++ src/main/scala/com/nygaardc/dixa/services/grpc/PrimeNumberServiceImlp.scala
@@ -1,16 +1,27 @@
 package com.nygaardc.dixa.services.grpc
 
 import akka.NotUsed
+import akka.grpc.GrpcServiceException
 import akka.stream.Materializer
 import akka.stream.scaladsl.Source
 import com.nygaardc.dixa.grpc.{PrimeNumberRequest, PrimeNumberResponse, PrimeNumberService}
+import io.grpc.Status
 
 class PrimeNumberServiceImlp(implicit mat: Materializer) extends PrimeNumberService {
   import mat.executionContext
 
   override def get(in: PrimeNumberRequest): Source[PrimeNumberResponse, NotUsed] = {
+    if (in.n < 2) {
+      return Source.failed(
+        new GrpcServiceException(
+          Status.INVALID_ARGUMENT.withDescription("numbers < 2 not allowed")
+        )
+      )
+    }
     val primeNumberGenerator = new PrimeNumberGenerator
-    val primes = primeNumberGenerator.getPrimes(in.n)
-    Source(primes) map {i => PrimeNumberResponse(i)}
+    val primes = primeNumberGenerator.getPrimes
+    Source(primes)
+      .takeWhile(_ <= in.n )
+      .map(i => PrimeNumberResponse(i))
   }
 }
